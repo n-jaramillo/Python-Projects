9261fe56568f837f34f874a963cd5d3e991e4994
n-jaramillo <102261871+n-jaramillo@users.noreply.github.com>
2023-01-19 05:31:13 -0800

    added instructions

---
+++ python_function_practice_part_4.py
@@ -0,0 +1,23 @@
+# Write a Python function called max_num()to find the maximum of three numbers.
+def max_num():
+    pass
+
+# Write a Python function called mult_list() to multiply all the numbers in a list.
+def mult_list():
+    pass
+
+# Write a Python function called rev_string() to reverse a string.
+def rev_string():
+    pass
+
+# Write a Python function called num_within() to check whether a number falls in a given range.
+    # The function accepts the number, beginning of range, and end of range(inclusive) as arguments.
+    # Examples: num_within(3, 2, 4) returns True, num_within(3, 1, 3) returns True, num_within(10, 2, 5) returns False.
+def num_within():
+    pass
+
+# Write a Python function called pascal() that prints out the first n rows of Pascal's triangle.
+    # The function accepts the number n, the number of rows to print
+    # Note: Pascal's triangle is an arithmetic and geometric figure first imagined by Blaise Pascal. Each number is the two numbers above it added together.
+def pascal():
+    pass
