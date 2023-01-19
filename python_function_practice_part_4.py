251ed48fb7ab99b744db1e95cb4415253cb2459c
n-jaramillo <102261871+n-jaramillo@users.noreply.github.com>
2023-01-19 05:51:54 -0800

    added num_within function that returns bool based on number in range

--- python_function_practice_part_4.py
+++ python_function_practice_part_4.py
@@ -26,8 +26,12 @@ print(rev_string("Hello World"))
 # Write a Python function called num_within() to check whether a number falls in a given range.
     # The function accepts the number, beginning of range, and end of range(inclusive) as arguments.
     # Examples: num_within(3, 2, 4) returns True, num_within(3, 1, 3) returns True, num_within(10, 2, 5) returns False.
-def num_within():
-    pass
+def num_within(n, range_start, range_end):
+    return n in range(range_start, range_end+1)
+
+print(num_within(3, 1, 3))
+print(num_within(3, 2, 4))
+print(num_within(10, 2, 5))
 
 # Write a Python function called pascal() that prints out the first n rows of Pascal's triangle.
     # The function accepts the number n, the number of rows to print
